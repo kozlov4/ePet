b431c658cbc9124e0f3d17f8aabfb971134981e6
DreamYwY <zaharywy@gmail.com>
2025-09-30 15:16:44 +0300

    Додано меню

--- android/app/src/main/java/com/example/epet/ui/main/view/ServicesFragment.kt
+++ android/app/src/main/java/com/example/epet/ui/main/view/ServicesFragment.kt
@@ -11,7 +11,13 @@ import androidx.navigation.fragment.findNavController
 import com.example.epet.R
 
 class ServicesFragment : Fragment() {
-    
+
+    private lateinit var tv_to_mail: TextView
+    private lateinit var tv_to_settings: TextView
+    private lateinit var tv_to_update_app: TextView
+    private lateinit var tv_to_support: TextView
+    private lateinit var tv_to_question: TextView
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.fragment_services, container, false)
     }
@@ -23,5 +29,10 @@ class ServicesFragment : Fragment() {
 
     /** Ініціалізація всіх елементів інтерфейсу **/
     private fun initViews(view: View) {
+        tv_to_mail = view.findViewById(R.id.tv_to_mail)
+        tv_to_settings = view.findViewById(R.id.tv_to_settings)
+        tv_to_update_app = view.findViewById(R.id.tv_to_update_app)
+        tv_to_support = view.findViewById(R.id.tv_to_support)
+        tv_to_question = view.findViewById(R.id.tv_to_question)
     }
 }
