9c3e7dea91fc6500d511087930f0718bfee65f9c
Serge Kozlov <dinogithub56@gmail.com>
2025-10-16 12:18:23 +0300

    Додав функцію для валійдації jwt

--- backend/src/api/core.py
+++ backend/src/api/core.py
@@ -1,10 +1,15 @@
 import os
 from datetime import timedelta, datetime, timezone
+from typing import Annotated
+from fastapi import Depends, HTTPException
 from passlib.context import CryptContext
 from jose import jwt, JWTError
+from starlette import status
+from fastapi.security import OAuth2PasswordBearer
 
 
 bcrypt_context = CryptContext(schemes=['bcrypt'], deprecated='auto')
+oauth2_bearer = OAuth2PasswordBearer(tokenUrl="/organizations/login")
 
 
 def create_access_token(subject: str, id: int, expires_delta: timedelta):
@@ -12,3 +17,16 @@ def create_access_token(subject: str, id: int, expires_delta: timedelta):
     expires = datetime.now(timezone.utc) + expires_delta
     encode.update({'exp': expires})
     return jwt.encode(encode, os.getenv("SECRET_KEY"), algorithm=os.getenv("ALGORITHM"))
+
+
+
+async def get_current_user(token: Annotated[str, Depends(oauth2_bearer)]):
+    try:
+        payload = jwt.decode(token,  os.getenv("SECRET_KEY"), os.getenv("ALGORITHM"))
+        username: str = payload.get('sub')
+        user_id: int = payload.get('id')
+        if username is None or user_id is None:
+            raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail='Could not validate user')
+        return {'username': username, 'user_id': user_id}
+    except JWTError:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail='Could not validate user')
