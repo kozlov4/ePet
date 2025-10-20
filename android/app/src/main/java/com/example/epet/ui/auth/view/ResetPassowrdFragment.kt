3bd1e69029b435abe64cc7099aa465320776ea1f
DreamYwY <zaharywy@gmail.com>
2025-10-20 13:01:53 +0300

    Змінена назва

--- android/app/src/main/java/com/example/epet/ui/auth/view/ResetPassowrdFragment.kt
+++ android/app/src/main/java/com/example/epet/ui/auth/view/ResetPassowrdFragment.kt
@@ -77,7 +77,7 @@ class ResetPassowrdFragment : Fragment() {
                         val action = ResetPassowrdFragmentDirections.actionResetPasswordToMessage(
                             tittletext = "Відновлення паролю",
                             emoji = "✅",
-                            main = "Інструкцію надіслано!",
+                            main = "Успішно!",
                             description = state,
                             email = et_email_address.text.toString()
                         )
