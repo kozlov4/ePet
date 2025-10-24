a1bf668caf9a42b9171d82bc624746e5b7b3755d
Rostyslav <rostyslav.tarasov@nure.ua>
2025-10-24 13:42:23 +0300

    small fix

--- web/pages/CNAP/home.tsx
+++ web/pages/CNAP/home.tsx
@@ -1,12 +1,21 @@
+import React, { useState, useEffect } from 'react';
 import { HeaderCNAP } from '../../components/CNAP/headerCNAP'
 import { MainCNAP } from '../../components/CNAP/mainCNAP'
 import { FooterCNAP } from '../../components/CNAP/footer'
 
 export default function CNAPHome() {
-    const user_name: string = localStorage.getItem('user_name');
+    const [userName, setUserName] = useState('');
+
+    useEffect(() => {
+        if (typeof window !== 'undefined') {
+            const storedUserName = localStorage.getItem('user_name') || '';
+            setUserName(storedUserName);
+        }
+    }, []);
+
     return (
         <div className="flex min-h-screen w-screen flex-col bg-cover bg-center">
-            <HeaderCNAP cnapid={user_name} />
+            <HeaderCNAP cnapid={userName} />
             <main className="flex-grow">
                 <MainCNAP />
             </main>
