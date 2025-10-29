01569a818e16092530e0f05d6ede3d848db19ee8
Serge Kozlov <dinogithub56@gmail.com>
2025-10-29 12:54:27 +0200

    fix 9 symbol in pass

--- backend/src/schemas/password_schemas.py
+++ backend/src/schemas/password_schemas.py
@@ -9,4 +9,4 @@ class ForgotPasswordPayload(BaseModel):
 
 class ResetPasswordPayload(BaseModel):
     token: str
-    new_password:str= Field(min_length=9, max_length=100)
+    new_password:str= Field(min_length=8, max_length=100)
