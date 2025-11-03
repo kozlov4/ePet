6607ec7a0657f6dec88248ef28f63e386f0481ca
Serge Kozlov <dinogithub56@gmail.com>
2025-11-03 17:36:30 +0200

    fix bugs in login func

--- backend/src/schemas/token_schemas.py
+++ backend/src/schemas/token_schemas.py
@@ -1,7 +1,7 @@
 from pydantic import BaseModel
-
+from typing import Optional
 class TokenResponse(BaseModel):
     access_token: str
     token_type: str
     user_name: str
-    organization_type:str
+    organization_type: Optional[str] = None
