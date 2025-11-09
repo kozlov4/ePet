ff7301e7b69b978af0d9afe42d8aa61c515708ae
Rostyslav <rostyslav.tarasov@nure.ua>
2025-11-09 17:23:34 +0200

    refactor: improve code formatting and consistency in Layout component

--- web/components/Base/Layout.tsx
+++ web/components/Base/Layout.tsx
@@ -1,21 +1,20 @@
-import React from 'react'
-import { Footer } from './Footer'
-import { Header } from './Header/Header'
-import { HeaderProps } from './Header/HeaderTypes'
-
+import React from 'react';
+import { Footer } from './Footer';
+import { Header } from './Header/Header';
+import { HeaderProps } from './Header/HeaderTypes';
 
 type LayoutProps = {
-    children: React.ReactNode
-    headerProps?: HeaderProps | null
-    showHeader?: boolean
-    customHeader?: React.ReactNode
-}
+    children: React.ReactNode;
+    headerProps?: HeaderProps | null;
+    showHeader?: boolean;
+    customHeader?: React.ReactNode;
+};
 
-export const Layout: React.FC<LayoutProps> = ({ 
-    children, 
+export const Layout: React.FC<LayoutProps> = ({
+    children,
     headerProps,
     showHeader = true,
-    customHeader
+    customHeader,
 }) => {
     const defaultHeaderProps: HeaderProps = {
         navProps: [
@@ -27,16 +26,17 @@ export const Layout: React.FC<LayoutProps> = ({
             label: 'Увійти до кабінету',
             href: '/signIn',
         },
-    }
+    };
 
-    const finalHeaderProps = headerProps || defaultHeaderProps
+    const finalHeaderProps = headerProps || defaultHeaderProps;
 
     return (
         <div className="w-screen min-h-screen">
             {showHeader && (customHeader || <Header {...finalHeaderProps} />)}
-            <main className="w-full max-w-[1340px] mx-auto flex-grow overflow-x-hidden">{children}</main>
+            <main className="w-full max-w-[1920px] mx-auto flex-grow overflow-x-hidden">
+                {children}
+            </main>
             <Footer />
         </div>
-    )
-}
-
+    );
+};
