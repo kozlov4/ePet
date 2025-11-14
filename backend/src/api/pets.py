b9b2310c974772d796c256a7578586176bf2d2e8
Serge Kozlov <serge.kozlov.dev@gmail.com>
2025-11-14 12:02:27 +0200

    rout for del pet is done

--- backend/src/api/pets.py
+++ backend/src/api/pets.py
@@ -284,3 +284,34 @@ async def add_pet(
             "chip_date": chip_date.isoformat()
         }
     }
+
+
+@router.delete("/delete/{pet_id}")
+async def delete_pet(
+    pet_id: int,
+    org_or_cnap: Annotated[Organizations | Cnap, Depends(get_current_org_or_cnap)],
+    db: db_dependency
+):
+
+    if isinstance(org_or_cnap, Organizations):
+        org_type = org_or_cnap.organization_type
+        org_id = org_or_cnap.organization_id
+    elif isinstance(org_or_cnap, Cnap):
+        org_type = "ЦНАП"
+        org_id = org_or_cnap.cnap_id
+    else:
+        raise HTTPException(status_code=403, detail="Недостатньо прав")
+
+    pet = db.query(Pets).filter(Pets.pet_id == pet_id).first()
+
+    if not pet:
+        raise HTTPException(status_code=404, detail="Тваринку не знайдено")
+
+    if org_type != "ЦНАП" and pet.organization_id != org_id:
+        raise HTTPException(status_code=403, detail="Недостатньо прав для видалення")
+
+    db.delete(pet)
+    db.commit()
+
+    return {"message": "Успішне видалення"}
+
