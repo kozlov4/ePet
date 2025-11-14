ba9dd1b35716306939d981e13556733ffd6575ba
DreamY <zaharywy@gmail.com>
2025-11-14 20:45:41 +0200

    Update ReusableTable.tsx

--- web/components/Base/ReusableTable.tsx
+++ web/components/Base/ReusableTable.tsx
@@ -100,7 +100,7 @@ export function ReusableTable({
 
     return (
         <motion.div
-            className="w-full px-4 md:px-10 py-10"
+            className="max-w-[1920px] w-full px-4 md:px-10 py-10"
             initial={{ opacity: 0, y: 30 }}
             animate={{ opacity: 1, y: 0 }}
             transition={{ duration: 0.6, ease: 'easeOut' }}
