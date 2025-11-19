789f29fa4fa54340207c89b4a899452ef3569e07
DreamYwY <zaharywy@gmail.com>
2025-11-19 19:21:06 +0200

    Added switch for extract data

--- android/app/src/main/java/com/example/epet/ui/service/view/ExtractPetFragment.kt
+++ android/app/src/main/java/com/example/epet/ui/service/view/ExtractPetFragment.kt
@@ -5,14 +5,20 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
+import android.widget.TextView
 import androidx.appcompat.widget.AppCompatButton
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import com.example.epet.R
+import androidx.navigation.fragment.navArgs
 
 class ExtractPetFragment : Fragment() {
 
+    private val args: ExtractPetFragmentArgs by navArgs()
+
     private lateinit var iv_to_back: ImageView
+    private lateinit var tv_tittletext: TextView
+    private lateinit var tv_description: TextView
     private lateinit var bth_create_extract: AppCompatButton
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
@@ -22,13 +28,31 @@ class ExtractPetFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         initViews(view)
+        initExtractInfo()
         initButtons()
     }
 
     /** Ініціалізація всіх елементів інтерфейсу **/
     private fun initViews(view: View) {
         iv_to_back = view.findViewById(R.id.iv_to_back)
+        tv_tittletext = view.findViewById(R.id.tv_tittletext)
+        tv_description = view.findViewById(R.id.tv_description)
         bth_create_extract = view.findViewById(R.id.bth_create_extract)
+    }
+
+    /** Ініціалізація даних витяга **/
+    private fun initExtractInfo() {
+        val extractName = args.extractName
+
+        if (extractName == "Витяг з реєстру домашніх тварин") {
+            tv_description.setText("• Ім’я тварини\n• Вид\n• Порода\n• Стать\n• Дата народження\n• Колір\n• Фото\n• Інформація про ЦНАП, який видав\n   паспорт\n• Інформація про власника")
+        } else if (extractName == "Витяг за паспортом тварини") {
+            tv_description.text = "• Номер паспорта\n• Дані ЦНАП, який видав паспорт\n• Ім'я\n• Вид\n• Порода\n• Дата народження"
+        } else if (extractName == "Витяг про щеплення тварини") {
+            tv_description.text = "• Список проведених щеплень\n• Виробник вакцини\n• Назва препарату\n• Серія\n• Дата вакцинації\n• Термін дії\n• Дані організації, яка проводили\n   щеплення"
+        } else if (extractName == "Витяг за ідентифікаторами") {
+            tv_description.text = "• Тип ідентифікатора\n• Номер\n• Дата встановлення\n• Дані тварини\n• Інформація про ЦНАП, що встановила\n   ідентифікатор"
+        }
     }
 
     /** Ініціалізація всіх кнопок інтерфейсу **/
