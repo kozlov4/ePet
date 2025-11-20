3dad5bf652c961ce89b1bfd55f0b9aef7f1b4120
DreamYwY <zaharywy@gmail.com>
2025-11-20 17:21:47 +0200

    One animal extract removed

--- android/app/src/main/java/com/example/epet/ui/service/view/ExtractListFragment.kt
+++ android/app/src/main/java/com/example/epet/ui/service/view/ExtractListFragment.kt
@@ -15,7 +15,6 @@ class ExtractListFragment : Fragment() {
     private lateinit var iv_to_back: ImageView
 
     private lateinit var cc_to_extract_pet_register: ConstraintLayout
-    private lateinit var cc_to_extract_pet_passport: ConstraintLayout
     private lateinit var cc_to_extract_pet_vaccinations: ConstraintLayout
     private lateinit var cc_to_extract_pet_identifier: ConstraintLayout
 
@@ -33,7 +32,6 @@ class ExtractListFragment : Fragment() {
     private fun initViews(view: View) {
         iv_to_back = view.findViewById(R.id.iv_to_back)
         cc_to_extract_pet_register = view.findViewById(R.id.cc_to_extract_pet_register)
-        cc_to_extract_pet_passport = view.findViewById(R.id.cc_to_extract_pet_passport)
         cc_to_extract_pet_vaccinations = view.findViewById(R.id.cc_to_extract_pet_vaccinations)
         cc_to_extract_pet_identifier = view.findViewById(R.id.cc_to_extract_pet_identifier)
     }
@@ -46,11 +44,6 @@ class ExtractListFragment : Fragment() {
 
         cc_to_extract_pet_register.setOnClickListener {
             val action = ExtractListFragmentDirections.actionExtractListToExtractPet("Витяг з реєстру домашніх тварин")
-            findNavController().navigate(action)
-        }
-
-        cc_to_extract_pet_passport.setOnClickListener {
-            val action = ExtractListFragmentDirections.actionExtractListToExtractPet("Витяг за паспортом тварини")
             findNavController().navigate(action)
         }
 
