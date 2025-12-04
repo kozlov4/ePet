6b06416bac424f26c9cbc32142d39723c8620906
DreamY <zaharywy@gmail.com>
2025-12-04 15:49:13 +0200

    Update reset-password.tsx

--- web/pages/reset-password.tsx
+++ web/pages/reset-password.tsx
@@ -1,6 +1,6 @@
 import { useSearchParams } from 'next/navigation';
-import { NewPasswordPage } from '../components/auth/NewPassword';
-import { ResetPasswordPage } from '../components/auth/ResetPassword';
+import { NewPasswordPage } from '../components/Auth/NewPassword';
+import { ResetPasswordPage } from '../components/Auth/ResetPassword';
 
 const ResetPassword = () => {
     const searchParams = useSearchParams();
